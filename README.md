c9084bf3b6f56534ffda2d7477c428c818e350d8
sudeepSubedi01 <sudip.subedi99@gmail.com>
2025-12-18 22:27:35 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FinGuide - Think Smart Spend Smart
---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "INITIAL COMMIT"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+
+
+print("Success")
